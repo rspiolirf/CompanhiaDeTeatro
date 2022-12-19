ce514db1b52dca1336b81270bfb2e8e06a24b90c
rspiolirf <rspioli.rf@gmail.com>
2022-12-19 17:29:52 -0300

    [Pioli] Ajustes nos namespaces

--- CompanhiaDeTeatro.Infraestrutura/BancoDeDados/PecaRepositorioEmMemoria.cs
+++ CompanhiaDeTeatro.Infraestrutura/BancoDeDados/PecaRepositorioEmMemoria.cs
@@ -3,30 +3,29 @@ using CompanhiaDeTeatro.CasosDeUso.Interfaces;
 using CompanhiaDeTeatro.Dominio.Entidades;
 using CompanhiaDeTeatro.Dominio.Enumeracoes;
 
-namespace CompanhiaDeTeatro.Infraestrutura.BancoDeDados
+namespace CompanhiaDeTeatro.Infraestrutura.BancoDeDados;
+
+public class PecaRepositorioEmMemoria : IPecaRepositorio
 {
-    public class PecaRepositorioEmMemoria : IPecaRepositorio
+    private readonly List<Peca> _pecas = new()
     {
-        private readonly List<Peca> pecas = new ()
-        {
-            new Peca() { CodigoPeca = 1, Descricao = "Hamlet", Genero = GeneroPeca.Tragedia },
-            new Peca() { CodigoPeca = 2, Descricao = "As You Like It", Genero = GeneroPeca.Comedia },
-            new Peca() { CodigoPeca = 3, Descricao = "Othello", Genero = GeneroPeca.Tragedia },
-        };
+        new Peca() { CodigoPeca = 1, Descricao = "Hamlet", Genero = GeneroPeca.Tragedia },
+        new Peca() { CodigoPeca = 2, Descricao = "As You Like It", Genero = GeneroPeca.Comedia },
+        new Peca() { CodigoPeca = 3, Descricao = "Othello", Genero = GeneroPeca.Tragedia },
+    };
 
-        public Peca ConsultaPorCodigo(int codigo)
-        {
-            var resultado = pecas.FirstOrDefault(p => p.CodigoPeca == codigo);
+    public Peca ConsultaPorCodigo(int codigo)
+    {
+        var resultado = _pecas.FirstOrDefault(p => p.CodigoPeca == codigo);
 
-            if (resultado is null)
-                throw new PecaNaoEncontradaException();
+        if (resultado is null)
+            throw new PecaNaoEncontradaException();
 
-            return resultado;
-        }
+        return resultado;
+    }
 
-        public IEnumerable<Peca> ConsultaTodas()
-        {
-            return pecas;
-        }
+    public IEnumerable<Peca> ConsultaTodas()
+    {
+        return _pecas;
     }
 }
